08958707ab445efe9113e3cdd6f5e9675fcf0981
Samir Talwar <samir@functional.computer>
2023-04-22 22:24:44 +0200

    Read from input.

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,27 @@ pub mod interpreter;
 pub mod parser;
 
 use crate::ast::*;
+use crate::error::BooError;
 use crate::interpreter::interpret;
+use crate::parser::parse;
 
-fn main() {
-    let expr = Expr::Infix {
-        annotation: (),
-        operation: Operation::Multiply,
-        left: Box::new(Expr::Infix {
-            annotation: (),
-            operation: Operation::Add,
-            left: Box::new(Expr::Primitive {
-                annotation: (),
-                value: Primitive::Int(2),
-            }),
-            right: Box::new(Expr::Primitive {
-                annotation: (),
-                value: Primitive::Int(3),
-            }),
-        }),
-        right: Box::new(Expr::Primitive {
-            annotation: (),
-            value: Primitive::Int(7),
-        }),
+fn main() -> Result<(), BooError> {
+    let mut args = std::env::args();
+    args.next();
+    let Some(input) = args.next() else {
+        panic!("No input.")
     };
+    let expr = parse(&input)?;
     let result = interpret(expr);
 
     match result {
         Expr::Primitive {
             annotation: _,
             value,
-        } => println!("{}", value),
+        } => {
+            println!("{}", value);
+            Ok(())
+        }
         result => panic!("Invalid result: {:?}", result),
     }
 }
