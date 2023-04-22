9581f11284af4727411fdf36796caa28656abaf4
Samir Talwar <samir@functional.computer>
2023-04-22 22:48:38 +0200

    Add a prompt with reedline.

--- src/error.rs
+++ src/error.rs
@@ -2,3 +2,11 @@
 pub enum BooError {
     ParseError(peg::error::ParseError<peg::str::LineCol>),
 }
+
+impl std::fmt::Display for BooError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ParseError(error) => write!(f, "Parse error: {}", error),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,38 @@ pub mod error;
 pub mod interpreter;
 pub mod parser;
 
+use reedline::*;
+
 use crate::ast::*;
-use crate::error::BooError;
 use crate::interpreter::interpret;
 use crate::parser::parse;
 
-fn main() -> Result<(), BooError> {
-    let mut args = std::env::args();
-    args.next();
-    let Some(input) = args.next() else {
-        panic!("No input.")
+fn main() {
+    let mut line_editor = Reedline::create();
+    let prompt = DefaultPrompt {
+        left_prompt: DefaultPromptSegment::Empty,
+        right_prompt: DefaultPromptSegment::Empty,
     };
-    let expr = parse(&input)?;
+
+    loop {
+        let sig = line_editor.read_line(&prompt);
+        match sig {
+            Ok(Signal::Success(buffer)) => match run(&buffer) {
+                Ok(()) => (),
+                Err(message) => eprintln!("{}", message),
+            },
+            Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
+                break;
+            }
+            Err(err) => {
+                eprintln!("Error: {}", err);
+            }
+        }
+    }
+}
+
+fn run(buffer: &str) -> Result<(), String> {
+    let expr = parse(buffer).map_err(|err| format!("{}", err))?;
     let result = interpret(expr);
 
     match result {
@@ -25,6 +45,6 @@ fn main() -> Result<(), BooError> {
             println!("{}", value);
             Ok(())
         }
-        result => panic!("Invalid result: {:?}", result),
+        result => Err(format!("Invalid result: {:?}", result)),
     }
 }
