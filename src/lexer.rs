0589c9f72f69536fefbee8897ecfd01e1b4bd986
Samir Talwar <samir@functional.computer>
2023-04-23 23:14:58 +0200

    Just collect the lexing; we need to backtrack to parse anyway.

--- src/lexer.rs
+++ src/lexer.rs
@@ -14,13 +14,16 @@ pub enum Token {
     Operator(String),
 }
 
-pub fn lex(input: &str) -> impl Iterator<Item = Result<Token, Error>> + '_ {
-    Token::lexer(input).spanned().map(move |(token, span)| {
-        token.map_err(|_| Error::UnexpectedToken {
-            span: span.clone().into(),
-            token: input[span].to_string(),
+pub fn lex(input: &str) -> Vec<Result<Token, Error>> {
+    Token::lexer(input)
+        .spanned()
+        .map(move |(token, span)| {
+            token.map_err(|_| Error::UnexpectedToken {
+                span: span.clone().into(),
+                token: input[span].to_string(),
+            })
         })
-    })
+        .collect()
 }
 
 #[cfg(test)]
@@ -30,7 +33,7 @@ mod tests {
     #[test]
     fn test_lexing_nothing() {
         let input = "";
-        let tokens = lex(input).collect::<Vec<_>>();
+        let tokens = lex(input);
 
         assert_eq!(tokens, vec![]);
     }
@@ -38,7 +41,7 @@ mod tests {
     #[test]
     fn test_lexing_an_integer() {
         let input = "123";
-        let tokens = lex(input).collect::<Vec<_>>();
+        let tokens = lex(input);
 
         assert_eq!(tokens, vec![Ok(Token::Integer(123))]);
     }
@@ -46,7 +49,7 @@ mod tests {
     #[test]
     fn test_lexing_a_negative_integer() {
         let input = "-456";
-        let tokens = lex(input).collect::<Vec<_>>();
+        let tokens = lex(input);
 
         assert_eq!(tokens, vec![Ok(Token::Integer(-456))]);
     }
@@ -54,7 +57,7 @@ mod tests {
     #[test]
     fn test_lexing_an_integer_with_underscores() {
         let input = "987_654_321";
-        let tokens = lex(input).collect::<Vec<_>>();
+        let tokens = lex(input);
 
         assert_eq!(tokens, vec![Ok(Token::Integer(987_654_321))]);
     }
@@ -62,24 +65,27 @@ mod tests {
     #[test]
     fn test_lexing_rejects_an_integer_starting_with_an_underscore() {
         let input = "_2";
-        let mut tokens = lex(input);
+        let tokens = lex(input);
 
         assert_eq!(
-            tokens.next(),
-            Some(Err(Error::UnexpectedToken {
-                span: miette::SourceSpan::new(
-                    miette::SourceOffset::from_location(input, 1, 1),
-                    1.into(),
-                ),
-                token: "_".to_string(),
-            }))
+            tokens,
+            vec![
+                Err(Error::UnexpectedToken {
+                    span: miette::SourceSpan::new(
+                        miette::SourceOffset::from_location(input, 1, 1),
+                        1.into(),
+                    ),
+                    token: "_".to_string(),
+                }),
+                Ok(Token::Integer(2)),
+            ]
         );
     }
 
     #[test]
     fn test_lexing_operators() {
         let input = "1 + 2 - 3 * 4";
-        let tokens = lex(input).collect::<Vec<_>>();
+        let tokens = lex(input);
 
         assert_eq!(
             tokens,
@@ -98,19 +104,21 @@ mod tests {
     #[test]
     fn test_lexing_rejects_anything_else() {
         let input = "1 / 2";
-        let mut tokens = lex(input);
-
-        assert_eq!(tokens.next(), Some(Ok(Token::Integer(1))));
+        let tokens = lex(input);
 
         assert_eq!(
-            tokens.next(),
-            Some(Err(Error::UnexpectedToken {
-                span: miette::SourceSpan::new(
-                    miette::SourceOffset::from_location(input, 1, 3),
-                    1.into(),
-                ),
-                token: "/".to_string(),
-            }))
+            tokens,
+            vec![
+                Ok(Token::Integer(1)),
+                Err(Error::UnexpectedToken {
+                    span: miette::SourceSpan::new(
+                        miette::SourceOffset::from_location(input, 1, 3),
+                        1.into(),
+                    ),
+                    token: "/".to_string(),
+                }),
+                Ok(Token::Integer(2)),
+            ]
         );
     }
 }
