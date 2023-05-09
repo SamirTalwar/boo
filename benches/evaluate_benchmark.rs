f25a71cf543153ee20da7fc9fa7dff92f494f605
Samir Talwar <samir@functional.computer>
2023-05-09 15:35:37 +0200

    Run the benchmarks with 16 different ASTs.

--- benches/evaluate_benchmark.rs
+++ benches/evaluate_benchmark.rs
@@ -6,17 +6,23 @@ use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criteri
 use boo::ast::*;
 use boo::*;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut unstructured_bytes = vec![0; 0x10000];
-    let mut rng = rand::rngs::StdRng::from_seed([0; 32]);
-    rng.fill_bytes(&mut unstructured_bytes);
-    let mut unstructured = arbitrary::Unstructured::new(&unstructured_bytes);
+fn evaluate_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("evaluate");
+    for seed_value in 0..16 {
+        let mut seed = [0; 32];
+        seed[0] = seed_value;
+        let mut rng = rand::rngs::StdRng::from_seed(seed);
+        let mut unstructured_bytes = vec![0; 0x10000];
+        rng.fill_bytes(&mut unstructured_bytes);
+        let mut unstructured = arbitrary::Unstructured::new(&unstructured_bytes);
 
-    let expr = Rc::new(unstructured.arbitrary::<Expr<()>>().unwrap());
-    c.bench_with_input(BenchmarkId::new("evaluate", &expr), &expr, |b, expr| {
-        b.iter(|| evaluate(black_box(expr.clone())))
-    });
+        let expr = Rc::new(unstructured.arbitrary::<Expr<()>>().unwrap());
+        group.bench_with_input(BenchmarkId::new("evaluate", &expr), &expr, |b, expr| {
+            b.iter(|| evaluate(black_box(expr.clone())))
+        });
+    }
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, evaluate_benchmark);
 criterion_main!(benches);
