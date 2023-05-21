a9a9591d0b103ccbb330aa56060e81b27865d9f8
Samir Talwar <samir@functional.computer>
2023-05-21 23:37:52 +0200

    Always run evaluation benchmarks on deep trees.
    Small trees are uninteresting.

--- crates/benchmarks/benches/evaluate_benchmark.rs
+++ crates/benchmarks/benches/evaluate_benchmark.rs
@@ -1,26 +1,28 @@
+use std::rc::Rc;
+
+use criterion::{black_box, BenchmarkId, Criterion};
 use proptest::strategy::{Strategy, ValueTree};
 use proptest::test_runner::TestRunner;
 
-use criterion::{black_box, BenchmarkId, Criterion};
-
+use boo::parser::generators::ExprGenConfig;
 use boo::*;
 
 pub fn evaluate_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("evaluate");
 
     let mut runner = TestRunner::deterministic();
-    for _ in 0..16 {
-        let tree = parser::generators::arbitrary()
-            .new_tree(&mut runner)
-            .unwrap();
+    for i in 0..8 {
+        let tree = parser::generators::gen(Rc::new(ExprGenConfig {
+            depth: 8..9,
+            ..Default::default()
+        }))
+        .new_tree(&mut runner)
+        .unwrap();
         let expr = tree.current();
-        let expr_string = format!("{}", expr);
         let pool = pool_exprs(expr);
-        group.bench_with_input(
-            BenchmarkId::new("evaluate", &expr_string),
-            &pool,
-            |b, pool| b.iter(|| evaluate(black_box(pool))),
-        );
+        group.bench_with_input(BenchmarkId::new("evaluate", i), &pool, |b, pool| {
+            b.iter(|| evaluate(black_box(pool)))
+        });
     }
     group.finish();
 }
