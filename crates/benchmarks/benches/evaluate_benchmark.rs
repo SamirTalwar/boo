e7421fe31e6a0813505b8a77b04c6eb3050c4372
Samir Talwar <samir@functional.computer>
2023-05-22 22:50:08 +0200

    Make a program that lists the benchmarks.

--- crates/benchmarks/benches/evaluate_benchmark.rs
+++ crates/benchmarks/benches/evaluate_benchmark.rs
@@ -1,28 +1,44 @@
+use std::iter;
 use std::rc::Rc;
 
 use criterion::{black_box, BenchmarkId, Criterion};
 use proptest::strategy::{Strategy, ValueTree};
 use proptest::test_runner::TestRunner;
 
+use boo::parser::ast::Expr;
 use boo::parser::generators::ExprGenConfig;
 use boo::*;
 
+const BENCHMARK_COUNT: usize = 8;
+
 pub fn evaluate_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("evaluate");
-
-    let mut runner = TestRunner::deterministic();
-    for i in 0..8 {
-        let tree = parser::generators::gen(Rc::new(ExprGenConfig {
-            depth: 8..9,
-            ..Default::default()
-        }))
-        .new_tree(&mut runner)
-        .unwrap();
-        let expr = tree.current();
+    for (i, expr) in benchmarks().take(BENCHMARK_COUNT).enumerate() {
         let pool = pool_exprs(expr);
         group.bench_with_input(BenchmarkId::new("evaluate", i), &pool, |b, pool| {
             b.iter(|| evaluate(black_box(pool)))
         });
     }
     group.finish();
+}
+
+#[allow(dead_code)]
+fn main() {
+    println!("Benchmarks for `evaluate`:");
+    for (i, expr) in benchmarks().take(BENCHMARK_COUNT).enumerate() {
+        println!("\nBenchmark #{}:\n{}", i, expr);
+    }
+}
+
+fn benchmarks() -> impl Iterator<Item = Expr> {
+    let mut runner = TestRunner::deterministic();
+    iter::from_fn(move || {
+        let tree = parser::generators::gen(Rc::new(ExprGenConfig {
+            depth: 8..9,
+            ..Default::default()
+        }))
+        .new_tree(&mut runner)
+        .unwrap();
+        Some(tree.current())
+    })
 }
