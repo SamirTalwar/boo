fa6a22cc9100f0a090903c39d2bcbc884173b8e2
Samir Talwar <samir@functional.computer>
2023-06-03 21:34:58 +0200

    Document `boo-test-helpers`.

--- crates/test-helpers/src/proptest.rs
+++ crates/test-helpers/src/proptest.rs
@@ -1,6 +1,9 @@
 use proptest::strategy::Strategy;
 use proptest::test_runner::{TestCaseResult, TestRunner};
 
+/// Runs a property-based test for the given strategy.
+///
+/// Similar to the `proptest!` macro, but this is not a macro.
 pub fn check<S: Strategy>(strategy: &S, test: impl Fn(S::Value) -> TestCaseResult) {
     let mut runner = TestRunner::default();
     match runner.run(strategy, test) {
