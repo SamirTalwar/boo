79657102be1e742d45aabeb97f6d6c1fe32f6cab
Samir Talwar <samir@functional.computer>
2023-07-16 17:54:41 +0200

    We no longer need to print pooled expressions.

--- crates/optimized-evaluator/src/ast.rs
+++ crates/optimized-evaluator/src/ast.rs
@@ -31,9 +31,3 @@ impl Expr {
 }
 
 impl Copy for Expr {}
-
-impl std::fmt::Display for Expr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
