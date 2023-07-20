a5262432cb171e179791b9e70b2dbdae53f99e41
Samir Talwar <samir@functional.computer>
2023-07-20 22:44:52 +0200

    Reduce the number of integer benchmarks.
    There isn't much difference between them.

--- crates/benchmarks/benches/primitive/integer_benchmark.rs
+++ crates/benchmarks/benches/primitive/integer_benchmark.rs
@@ -12,44 +12,40 @@ pub fn integer_benchmark(c: &mut Criterion) {
 
     {
         let mut group = c.benchmark_group("integer/small");
-        for _ in 0..4 {
-            let x = any::<i16>().new_tree(&mut runner).unwrap().current();
-            let y = any::<i16>().new_tree(&mut runner).unwrap().current();
-            group.bench_with_input(
-                BenchmarkId::new("Integer", format!("{} + {}", x, y)),
-                &(Integer::from(x), Integer::from(y)),
-                |b, (x, y)| b.iter(|| black_box(x) + black_box(y)),
-            );
-            group.bench_with_input(
-                BenchmarkId::new("i32", format!("{} + {}", x, y)),
-                &(i32::from(x), i32::from(y)),
-                |b, (x, y)| b.iter(|| black_box(x) + black_box(y)),
-            );
-            group.bench_with_input(
-                BenchmarkId::new("BigInt", format!("{} + {}", x, y)),
-                &(BigInt::from(x), BigInt::from(y)),
-                |b, (x, y)| b.iter(|| black_box(x) + black_box(y)),
-            );
-        }
+        let x = any::<i16>().new_tree(&mut runner).unwrap().current();
+        let y = any::<i16>().new_tree(&mut runner).unwrap().current();
+        group.bench_with_input(
+            BenchmarkId::new("Integer", format!("{} + {}", x, y)),
+            &(Integer::from(x), Integer::from(y)),
+            |b, (x, y)| b.iter(|| black_box(x) + black_box(y)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("i32", format!("{} + {}", x, y)),
+            &(i32::from(x), i32::from(y)),
+            |b, (x, y)| b.iter(|| black_box(x) + black_box(y)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("BigInt", format!("{} + {}", x, y)),
+            &(BigInt::from(x), BigInt::from(y)),
+            |b, (x, y)| b.iter(|| black_box(x) + black_box(y)),
+        );
         group.finish();
     }
 
     {
         let mut group = c.benchmark_group("integer/large");
-        for _ in 0..4 {
-            let x = any::<i128>().new_tree(&mut runner).unwrap().current();
-            let y = any::<i128>().new_tree(&mut runner).unwrap().current();
-            group.bench_with_input(
-                BenchmarkId::new("Integer", format!("{} * {}", x, y)),
-                &(Integer::from(x), Integer::from(y)),
-                |b, (x, y)| b.iter(|| black_box(x) * black_box(y)),
-            );
-            group.bench_with_input(
-                BenchmarkId::new("BigInt", format!("{} * {}", x, y)),
-                &(BigInt::from(x), BigInt::from(y)),
-                |b, (x, y)| b.iter(|| black_box(x) * black_box(y)),
-            );
-        }
+        let x = any::<i128>().new_tree(&mut runner).unwrap().current();
+        let y = any::<i128>().new_tree(&mut runner).unwrap().current();
+        group.bench_with_input(
+            BenchmarkId::new("Integer", format!("{} * {}", x, y)),
+            &(Integer::from(x), Integer::from(y)),
+            |b, (x, y)| b.iter(|| black_box(x) * black_box(y)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("BigInt", format!("{} * {}", x, y)),
+            &(BigInt::from(x), BigInt::from(y)),
+            |b, (x, y)| b.iter(|| black_box(x) * black_box(y)),
+        );
         group.finish();
     }
 }
