b982f4d4ce4df111dd028d758b90322b038cd7af
Samir Talwar <samir@functional.computer>
2023-08-06 22:03:00 +0200

    Factor out parts of the parser into functions.

--- crates/parser/src/parser.rs
+++ crates/parser/src/parser.rs
@@ -12,42 +12,9 @@ peg::parser! {
         pub rule root() -> Expr = e:expr() { e }
 
         pub rule expr() -> Expr = precedence! {
-            fn_:(quiet! { [AnnotatedToken { annotation: _, token: Token::Fn }] } / expected!("fn"))
-            parameters_:(quiet! { [AnnotatedToken { annotation: _, token: Token::Identifier(name) }] } / expected!("an identifier"))+
-            (quiet! { [AnnotatedToken { annotation: _, token: Token::Arrow }] } / expected!("->"))
-            body:expr() {
-                let span = fn_.annotation | body.span;
-                let parameters = parameters_.into_iter().map(|parameter|
-                    match &parameter.token {
-                        Token::Identifier(identifier) => identifier.clone(),
-                        _ => unreachable!(),
-                    }
-                ).collect();
-                Expr::new(fn_.annotation | body.span, Expression::Function(Function {
-                    parameters,
-                    body,
-                }))
-            }
+            x:function() { x }
             --
-            let_:(quiet! { [AnnotatedToken { annotation: _, token: Token::Let }] } / expected!("let"))
-            name:(quiet! { [AnnotatedToken { annotation: _, token: Token::Identifier(name) }] } / expected!("an identifier"))
-            (quiet! { [AnnotatedToken { annotation: _, token: Token::Assign }] } / expected!("="))
-            value:expr()
-            (quiet! { [AnnotatedToken { annotation: _, token: Token::In }] } / expected!("in"))
-            inner:expr() {
-                let n = match &name.token {
-                    Token::Identifier(name) => name,
-                    _ => unreachable!(),
-                };
-                Expr::new(
-                    let_.annotation | inner.span,
-                    Expression::Assign(Assign {
-                        name: n.clone(),
-                        value,
-                        inner,
-                    }),
-                )
-            }
+            x:assign() { x }
             --
             left:(@) (quiet! { [AnnotatedToken { annotation: _, token: Token::Operator("+") }] } / expected!("'+'")) right:@ {
                 construct_infix(left, Operation::Add, right)
@@ -70,7 +37,7 @@ peg::parser! {
                 )
             }
             --
-            a:atomic_expr() { a }
+            x:atomic_expr() { x }
         }
 
         rule atomic_expr() -> Expr =
@@ -98,6 +65,45 @@ peg::parser! {
                     Expression::Identifier(name.clone()),
                 )
             } } / expected!("an identifier")
+
+        rule function() -> Expr =
+            fn_:(quiet! { [AnnotatedToken { annotation: _, token: Token::Fn }] } / expected!("fn"))
+            parameters_:(quiet! { [AnnotatedToken { annotation: _, token: Token::Identifier(name) }] } / expected!("an identifier"))+
+            (quiet! { [AnnotatedToken { annotation: _, token: Token::Arrow }] } / expected!("->"))
+            body:expr() {
+                let span = fn_.annotation | body.span;
+                let parameters = parameters_.into_iter().map(|parameter|
+                    match &parameter.token {
+                        Token::Identifier(identifier) => identifier.clone(),
+                        _ => unreachable!(),
+                    }
+                ).collect();
+                Expr::new(fn_.annotation | body.span, Expression::Function(Function {
+                    parameters,
+                    body,
+                }))
+            }
+
+        rule assign() -> Expr =
+            let_:(quiet! { [AnnotatedToken { annotation: _, token: Token::Let }] } / expected!("let"))
+            name:(quiet! { [AnnotatedToken { annotation: _, token: Token::Identifier(name) }] } / expected!("an identifier"))
+            (quiet! { [AnnotatedToken { annotation: _, token: Token::Assign }] } / expected!("="))
+            value:expr()
+            (quiet! { [AnnotatedToken { annotation: _, token: Token::In }] } / expected!("in"))
+            inner:expr() {
+                let n = match &name.token {
+                    Token::Identifier(name) => name,
+                    _ => unreachable!(),
+                };
+                Expr::new(
+                    let_.annotation | inner.span,
+                    Expression::Assign(Assign {
+                        name: n.clone(),
+                        value,
+                        inner,
+                    }),
+                )
+            }
     }
 }
 
