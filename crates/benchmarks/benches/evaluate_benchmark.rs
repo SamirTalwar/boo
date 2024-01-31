436b659830a6d749cf9d56b22361f3fba101e9a3
Samir Talwar <samir@functional.computer>
2024-02-01 00:29:19 +0100

    Add benchmarks for the recursive evaluator (without pooling).
    And fix the benchmarks to add builtins and fail if evaluation fails.

--- crates/benchmarks/benches/evaluate_benchmark.rs
+++ crates/benchmarks/benches/evaluate_benchmark.rs
@@ -4,27 +4,42 @@ use criterion::{black_box, BenchmarkId, Criterion};
 use proptest::strategy::{Strategy, ValueTree};
 use proptest::test_runner::TestRunner;
 
+use boo_core::builtins;
 use boo_core::evaluation::Evaluator;
 use boo_core::expr::Expr;
 
 const BENCHMARK_COUNT: usize = 8;
 
 pub fn evaluate_benchmark(c: &mut Criterion) {
-    let reducing_evaluator = boo_evaluation_reduction::new();
-    let optimized_evaluator = boo_evaluation_optimized::new();
+    let evaluators: Vec<(String, Box<dyn Evaluator>)> = vec![
+        (
+            "reduction".to_owned(),
+            prepare(boo_evaluation_reduction::new()),
+        ),
+        (
+            "recursive".to_owned(),
+            prepare(boo_evaluation_recursive::new()),
+        ),
+        (
+            "optimized".to_owned(),
+            prepare(boo_evaluation_optimized::new()),
+        ),
+    ];
 
     let mut group = c.benchmark_group("evaluate");
     for (i, expr) in benchmarks().take(BENCHMARK_COUNT).enumerate() {
-        group.bench_with_input(
-            BenchmarkId::new("optimally evaluate", i),
-            &expr,
-            |b, expr| b.iter(|| optimized_evaluator.evaluate(black_box(expr.clone()))),
-        );
-        group.bench_with_input(BenchmarkId::new("naively evaluate", i), &expr, |b, expr| {
-            b.iter(|| reducing_evaluator.evaluate(black_box(expr.clone())))
-        });
+        for (name, evaluator) in evaluators.iter() {
+            group.bench_with_input(BenchmarkId::new(name, i), &expr, |b, expr| {
+                b.iter(|| evaluator.evaluate(black_box(expr.clone())).unwrap())
+            });
+        }
     }
     group.finish();
+}
+
+fn prepare(mut evaluator: impl Evaluator + 'static) -> Box<dyn Evaluator> {
+    builtins::prepare(&mut evaluator).unwrap();
+    Box::new(evaluator)
 }
 
 #[allow(dead_code)]
